e3e6f1e5d53bec72214d0511aa9da00d887a3150
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-13 13:22:55 +0530

    nothing changed yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import ViewFlights from "./components/ViewFlight"
+import { useState } from 'react'
+import AddFlight from './components/AddFlight'
 
 function App() {
   return (
     <>
-    </>
+    <ViewFlights/>
+    <AddFlight/>
+</>
   )
 }
 
